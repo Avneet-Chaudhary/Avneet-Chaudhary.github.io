9ecdc630d6de2bcb8b0273af8b60249e26aa0499
Avneet <93037547+Avneet-Chaudhary@users.noreply.github.com>
2022-04-21 08:27:18 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,44 +1,37 @@
-<?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$human = intval($_POST['human']);
-		$from = $email; 
-		
-		// WARNING: Be sure to change this. This is the address that the email will be sent to
-		$to = 'avneetchaudhary8273@gmail.com'; 
-		
-		$subject = "Message from ".$name." ";
-		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
- 
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
-		//Check if simple anti-bot test is correct
-		if ($human !== 5) {
-			$errHuman = 'Your anti-spam is incorrect';
-		}
- 
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-	}
-}
-	}
-?>
+ <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "contact";
+
+        $conn = mysqli_connect($servername, $username, $password, $database);
+        if (!$conn) {
+            die("Sorry we failed to connect: " . mysqli_connect_error());
+        } else {
+            // echo "Connection was successful<br>";
+           
+            $sql = "INSERT INTO `contactus` (`name`, `email`, `subject`, `message`) VALUES ('$name', '$email', '$subject', '$message')";
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                // echo "The record has been inserted successfully!";
+                echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Success!</strong> Thank You! your entry has been submitted successfully,i will be in touch.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+            } else {
+                // echo "The record has not inserted successfully because of this error ----->" . mysqli_error($conn);
+                echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> We are facing some technical issue, your entry is not submitted successfully! We regret the inconvinience caused.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+            }
+        }
+    }
+    ?>
